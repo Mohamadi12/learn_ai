0ff32c3824c4b720aa707ba66b8b447ef5aef1f1
Nana Mohamadi <adilnana78@gmail.com>
2025-08-11 15:06:10 +0000

    Discuter avec l'IA

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,30 @@
+import { openai } from "@ai-sdk/openai";
+import { anthropic } from "@ai-sdk/anthropic";
+import { convertToModelMessages, streamText, UIMessage } from "ai";
+
+export async function POST(req: Request) {
+  try {
+    const { messages }: { messages: UIMessage[] } = await req.json();
+
+    const result = streamText({
+      // model: anthropic("claude-sonnet-4-20250514"),
+      model: openai("gpt-4.1-nano"),
+      messages: convertToModelMessages(messages),
+    });
+
+    // Voir les tokens utilisés après le streaming est terminé
+    result.usage.then((usage) => {
+      console.log({
+        messageCount: messages.length,
+        inputTokens: usage.inputTokens,
+        outputTokens: usage.outputTokens,
+        totalTokens: usage.totalTokens,
+      });
+    });
+
+    return result.toUIMessageStreamResponse();
+  } catch (error) {
+    console.error("Error streaming text:", error);
+    return new Response("Failed to stream text", { status: 500 });
+  }
+}
