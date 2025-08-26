465d2b846d6b235b5a38a582b7878bc3990269e6
Nana Mohamadi <adilnana78@gmail.com>
2025-08-26 09:16:07 +0000

    Appels d'outils en plusieurs étapes

---
+++ src/app/api/multiple-tools/route.ts
@@ -0,0 +1,111 @@
+import {
+  UIMessage,
+  UIDataTypes,
+  streamText,
+  tool,
+  convertToModelMessages,
+  stepCountIs,
+  InferUITools,
+} from "ai";
+import { openai } from "@ai-sdk/openai";
+import { z } from "zod";
+
+const tools = {
+  getLocation: tool({
+    description: "Get the location of a user",
+    inputSchema: z.object({
+      name: z.string().describe("The name of the user"),
+    }),
+    execute: async ({ name }) => {
+      if (name === "Bruce Wayne") {
+        return "Gotham City";
+      } else if (name === "Clark Kent") {
+        return "Metropolis";
+      } else {
+        return "Unknown";
+      }
+    },
+  }),
+  getWeather: tool({
+    description: "Get the weather for a location",
+    inputSchema: z.object({
+      city: z.string().describe("The city to get the weather for"),
+    }),
+    execute: async ({ city }) => {
+      if (city === "Gotham City") {
+        return "70°F and cloudy";
+      } else if (city === "Metropolis") {
+        return "80°F and sunny";
+      } else {
+        return "Unknown";
+      }
+    },
+  }),
+};
+
+/*
+export const getLocation = tool({
+  description: "Get the location of a user",
+  inputSchema: z.object({
+    name: z.string().describe("The name of the user"),
+  }),
+  execute: async ({ name }) => {
+    if (name === "Bruce Wayne") {
+      return "Gotham City";
+    } else if (name === "Clark Kent") {
+      return "Metropolis";
+    } else {
+      return "Unknown";
+    }
+  },
+});
+
+Puis
+
+export const getWeather = tool({
+  description: "Get the weather for a location",
+  inputSchema: z.object({
+    city: z.string().describe("The city to get the weather for"),
+  }),
+  execute: async ({ city }) => {
+    if (city === "Gotham City") {
+      return "70°F and cloudy";
+    } else if (city === "Metropolis") {
+      return "80°F and sunny";
+    } else {
+      return "Unknown";
+    }
+  },
+});
+
+Puis on les apples comme :
+// tools/index.ts
+import { getLocation } from "./getLocation";
+import { getWeather } from "./getWeather";
+
+export const tools = {
+  getLocation,
+  getWeather,
+};
+*/
+
+export type ChatTools = InferUITools<typeof tools>;
+export type ChatMessage = UIMessage<never, UIDataTypes, ChatTools>;
+
+export async function POST(req: Request) {
+  try {
+    const { messages }: { messages: ChatMessage[] } = await req.json();
+
+    const result = streamText({
+      model: openai("gpt-5-mini"),
+      messages: convertToModelMessages(messages),
+      stopWhen: stepCountIs(3),
+      tools,
+    });
+
+    return result.toUIMessageStreamResponse();
+  } catch (error) {
+    console.error("Error streaming chat completion:", error);
+    return new Response("Failed to stream chat completion", { status: 500 });
+  }
+}
